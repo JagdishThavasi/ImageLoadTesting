eafbaafc256bc06ee661c89c75f07cb99da5f510
JagdishThavasi <jagiyadav@gmail.com>
2023-06-18 23:59:34 -0500

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -23,10 +23,10 @@ export const options = {
 
 export default function () {
     //const res = http.get('https://test.k6.io');
-    console.log('scene7-url-3');
+    console.log('scene7-url-9');
     sharedData.forEach(e => {
         //console.log(e['speedsize-url'])
-        let req = scene7_domain + e['scene7-url-3']
+        let req = scene7_domain + e['scene7-url-9']
         let res = http.get(req);
     });
     //console.log(sharedData.length)
